ba246af3fe403c09bcfe68e68d124d14c7a4b338
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-06 22:15:26 +0900

    Add new `MODULE` `raytracing_in_one_weekend`

---
+++ src/InOneWeekend/module.f90
@@ -0,0 +1,26 @@
+module raytracing_in_one_weekend
+
+    use, intrinsic :: iso_fortran_env
+
+
+
+    implicit none
+
+
+
+    private
+    public  :: render_image01
+
+
+
+    contains
+
+
+
+    subroutine render_image01
+
+        print *, "TEST: image 01"
+
+    end subroutine render_image01
+
+end module raytracing_in_one_weekend
--- src/main.f90
+++ src/main.f90
@@ -1,12 +1,13 @@
 program raytracing
 
-    use, intrinsic :: iso_fortran_env
+    use,     intrinsic :: iso_fortran_env
+    use, non_intrinsic :: raytracing_in_one_weekend
 
     implicit none
 
     print *, compiler_version()
     print *, compiler_options()
 
-    print *, "TEST"
+    call render_image01
 
 end program raytracing
