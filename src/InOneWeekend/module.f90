ef845611da61f7c4ff16ea6cf064483da2fbfc3d
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-06 22:38:05 +0900

    Add Implementation: Image 01

--- src/InOneWeekend/module.f90
+++ src/InOneWeekend/module.f90
@@ -19,7 +19,70 @@ module raytracing_in_one_weekend
 
     subroutine render_image01
 
-        print *, "TEST: image 01"
+        integer, parameter :: image_height = 256
+        integer, parameter :: image_width  = 256
+        integer, parameter :: white_point  = 255
+
+        integer, parameter :: iter_h_max   = image_height - 1
+        integer, parameter :: iter_w_max   = image_width  - 1
+
+
+
+        integer :: iter_h
+        !! A support variable for this `SUBROUTINE`
+
+        integer :: iter_w
+        !! A support variable for this `SUBROUTINE`
+
+        integer :: write_unit
+        !! the unit number to save the rendered image
+
+
+
+        open( &!
+            newunit = write_unit    , &!
+            file    = 'image01.ppm' , &!
+            action  = 'write'       , &!
+            form    = 'formatted'   , &!
+            status  = 'unknown'       &!
+        )
+
+
+
+        write( unit = write_unit, fmt = '(A)'        ) 'P3'
+        write( unit = write_unit, fmt = '(I0,1X,I0)' ) image_width, image_height
+        write( unit = write_unit, fmt = '(I0)'       ) white_point
+
+
+
+        do iter_h = 0, iter_h_max
+        do iter_w = 0, iter_w_max
+        block
+
+            real(real64) :: red, green, blue
+            !! red/green/blue components are represented internally by real-valued variables
+            !! that range from 0.0 to 1.0
+
+
+
+            red   = real( iter_w, kind = real64 ) / iter_w_max
+            green = real( iter_h, kind = real64 ) / iter_h_max
+            blue  = 0.0_real64
+
+
+
+            write( unit = write_unit, fmt = '(I0,2(1X,I0))' ) &!
+                floor( white_point * red   ) , &!
+                floor( white_point * green ) , &!
+                floor( white_point * blue  )
+
+        end block
+        end do
+        end do
+
+
+
+        close( write_unit )
 
     end subroutine render_image01
 
