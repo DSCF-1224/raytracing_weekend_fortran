8c46439b6b5576fa7d08659eaedc3d0c5fdf611c
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-07 06:59:16 +0900

    Update `MODULE` `raytracing_color` - Add an new `interface` `write(formatted)` for `color_type`

--- src/Color/module.f90
+++ src/Color/module.f90
@@ -13,10 +13,96 @@ module raytracing_color
 
 
 
+    interface write(formatted)
+        module procedure :: write_color
+    end interface write(formatted)
+
+
+
     type :: color_type
 
         real(real64), public :: red, green, blue
 
     end type color_type
+
+
+
+    integer, parameter, private :: white_point = 255
+
+
+
+    contains
+
+
+
+    subroutine write_color( color, unit, iotype, arglist, iostat, iomsg )
+
+        class(color_type), intent(in) :: color
+
+        integer, intent(in) :: unit
+
+        character(*), intent(in) :: iotype
+
+        integer, intent(in), dimension(:) :: arglist
+
+        integer, intent(out) :: iostat
+
+        character(*), intent(inout) :: iomsg
+
+
+
+        logical :: flag
+
+
+
+        flag =     ( iotype             .eq. 'LISTDIRECTED' ) &!
+        &    .and. ( size( arglist(:) ) .lt. 1              )
+
+
+
+        if ( flag ) then
+
+            call write_color_default( &!
+                color  = color    , &!
+                unit   = unit     , &!
+                iostat = iostat   , &!
+                iomsg  = iomsg(:)   &!
+            )
+
+            return
+
+        end if
+
+
+
+        error stop ' Unsupported `write(formatted)` mode was detected.'
+
+    end subroutine write_color
+
+
+
+    subroutine write_color_default( color, unit, iostat, iomsg )
+
+        class(color_type), intent(in) :: color
+
+        integer, intent(in) :: unit
+
+        integer, intent(out) :: iostat
+
+        character(*), intent(inout) :: iomsg
+
+
+
+        write( &!
+            unit   = unit            , &!
+            fmt    = '(I3,2(1X,I3))' , &!
+            iostat = iostat          , &!
+            iomsg  = iomsg(:)          &!
+        ) &!
+            floor( white_point * color%red   ) , &!
+            floor( white_point * color%green ) , &!
+            floor( white_point * color%blue  )
+
+    end subroutine write_color_default
 
 end module raytracing_color
