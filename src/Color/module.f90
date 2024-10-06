4cc729056bff12f844812e085e70c7897bc6cf63
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-07 06:28:16 +0900

    Update `MODULE` `raytracing_color` - Add a new `TYPE` `color_type`

--- src/Color/module.f90
+++ src/Color/module.f90
@@ -1,2 +1,22 @@
 module raytracing_color
+
+    use, intrinsic :: iso_fortran_env
+
+
+
+    implicit none
+
+
+
+    private
+    public  :: color_type
+
+
+
+    type :: color_type
+
+        real(real64), public :: red, green, blue
+
+    end type color_type
+
 end module raytracing_color
