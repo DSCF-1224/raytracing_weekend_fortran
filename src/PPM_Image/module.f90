dd27f28cab8c4710a875bedd6e501ea9c7171897
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-08 06:50:34 +0900

    Update `MODULE` `raytracing_ppm_image` - Add `SUBROUTINE` `write_ppm_image_header`

--- src/PPM_Image/module.f90
+++ src/PPM_Image/module.f90
@@ -1,2 +1,29 @@
 module raytracing_ppm_image
+
+    use, intrinsic :: iso_fortran_env
+
+
+
+    implicit none
+
+
+
+    contains
+
+
+
+    subroutine write_ppm_image_header( unit, image_width, image_height, white_point )
+
+        integer, intent(in) :: unit
+
+        integer, intent(in) :: image_width, image_height, white_point
+
+
+
+        write( unit = unit, fmt = '(A)'        ) 'P3'
+        write( unit = unit, fmt = '(I0,1X,I0)' ) image_width, image_height
+        write( unit = unit, fmt = '(I0)'       ) white_point
+
+    end subroutine write_ppm_image_header
+
 end module raytracing_ppm_image
