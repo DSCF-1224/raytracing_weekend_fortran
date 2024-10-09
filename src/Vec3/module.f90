90e7bf9215a1c59a770c379f99cf756883fed43a
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-10 07:03:31 +0900

    Update `MODULE` `raytracing_vec3` - Add a new `TYPE` `vec3_type`

--- src/Vec3/module.f90
+++ src/Vec3/module.f90
@@ -1,2 +1,22 @@
 module raytracing_vec3
+
+    use, intrinsic :: iso_fortran_env
+
+
+
+    implicit none
+
+
+
+    private
+    public  :: vec3_type
+
+
+
+    type :: vec3_type
+
+        real(real64), public :: x, y, z
+
+    end type vec3_type
+
 end module raytracing_vec3
