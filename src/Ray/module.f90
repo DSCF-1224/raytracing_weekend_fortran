b50ec8436c4eee33694bba56b735e4adbff71e7e
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-12 23:08:45 +0900

    Update `MODULE` `raytracing_ray` - Add a new `TYPE` `ray_type`

---
+++ src/Ray/module.f90
@@ -0,0 +1,19 @@
+module raytracing_ray
+
+    use, intrinsic :: iso_fortran_env
+
+
+
+    implicit none
+
+
+
+    private
+    public  :: ray_type
+
+
+
+    type :: ray_type
+    end type ray_type
+
+end module raytracing_ray
