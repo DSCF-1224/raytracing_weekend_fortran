8b10b6672663495e273d13ee3b105d1c94b09a2f
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-12 10:22:16 +0900

    Update `MODULE` `raytracing_vec3` - Update an `INTERFACE` `operator(*)`

--- src/Vec3/mul.f90
+++ src/Vec3/mul.f90
@@ -8,6 +8,16 @@ submodule (raytracing_vec3) imp_mul
 
 
 
+    module procedure mul_real64_vec3
+
+        new_vec3%x = lhs * rhs%x
+        new_vec3%y = lhs * rhs%y
+        new_vec3%z = lhs * rhs%z
+
+    end procedure mul_real64_vec3
+
+
+
     module procedure mul_vec3_real64
 
         new_vec3%x = lhs%x * rhs
