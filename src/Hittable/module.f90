08a8aee98f5cd5eaec227a8cf772c56b305f6d49
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-14 13:39:14 +0900

    Update `MODULE` `raytracing_hittable` - Introduce `module` `raytracing_hit_record` - Introduce `module` `raytracing_ray`

--- src/Hittable/module.f90
+++ src/Hittable/module.f90
@@ -1,5 +1,11 @@
 module raytracing_hittable
 
+    use,     intrinsic :: iso_fortran_env
+    use, non_intrinsic :: raytracing_hit_record
+    use, non_intrinsic :: raytracing_ray
+
+
+
     implicit none
 
 
