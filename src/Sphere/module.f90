7be4975016f219dfdd7765a450f127a723bbdac3
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-14 14:00:24 +0900

    Update `MODULE` `raytracing_sphere` - Introduce `module` `raytracing_vec3` - Add fields of the `sphere_type`

--- src/Sphere/module.f90
+++ src/Sphere/module.f90
@@ -1,2 +1,23 @@
 module raytracing_sphere
+
+    use,     intrinsic :: iso_fortran_env
+    use, non_intrinsic :: raytracing_vec3
+
+    implicit none
+
+
+
+    private
+    public  :: sphere_type
+
+
+
+    type :: sphere_type
+
+        real(real64), public :: radius
+
+        type(vec3_type), public :: center
+
+    end type sphere_type
+
 end module raytracing_sphere
