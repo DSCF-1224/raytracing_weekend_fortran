489b83b42a0d7b5208ef61cbeadf1afa631592fe
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-14 16:14:12 +0900

    Fix Implementation: Image 04 (mod1) - Merge updated `MODULE` `raytracing_hit_record` - Merge updated `MODULE` `raytracing_vec3` - Merge updated `MODULE` `raytracing_sphere`

--- src/Sphere/hit.f90
+++ src/Sphere/hit.f90
@@ -8,51 +8,69 @@ submodule (raytracing_sphere) imp_hit
 
     module procedure hit_sphere
 
-        real(real64) :: a, c, discriminant, h, root, sqrt_discriminant
-
-        type(vec3_type) :: oc
+        real(real64) :: root
 
 
 
-        hit_stat     = .false.
+        hit_stat = .false.
 
 
 
-        oc           = self%center - ray%origin
-        a            = length_squared( ray%direction )
-        h            = dot_product( ray%direction , oc )
-        c            = length_squared( oc ) - self%radius * self%radius
-        discriminant = h * h - a * c
+        block
+
+            real(real64) :: a, c, discriminant, h, sqrt_discriminant
+
+            type(vec3_type) :: oc
 
 
 
-        if ( discriminant .lt. 0.0_real64 ) return
+            oc           = self%center - ray%origin
+            a            = length_squared( ray%direction )
+            h            = dot_product( ray%direction , oc )
+            c            = length_squared( oc ) - self%radius * self%radius
+            discriminant = h * h - a * c
 
 
 
-        sqrt_discriminant = sqrt( discriminant )
-        root              = ( h - sqrt_discriminant ) / a
+            if ( discriminant .lt. 0.0_real64 ) return
+    
+    
+    
+            sqrt_discriminant = sqrt( discriminant )
+            root              = ( h - sqrt_discriminant ) / a
 
 
-
-        if ( ( root .le. t_min ) .or. ( t_max .le. root ) ) then
-
-            root = ( h + sqrt_discriminant ) / a
 
             if ( ( root .le. t_min ) .or. ( t_max .le. root ) ) then
-
-                return
-
+    
+                root = ( h + sqrt_discriminant ) / a
+    
+                if ( ( root .le. t_min ) .or. ( t_max .le. root ) ) then
+    
+                    return
+    
+                end if
+    
             end if
 
-        end if
+        end block
 
 
 
-        hit_stat          = .true.
-        hit_record%t      = root
-        hit_record%point  = ray%at( root )
-        hit_record%normal = ( hit_record%point - self%center ) / self%radius
+        block
+
+            type(vec3_type) :: outer_normal
+
+
+
+            hit_stat         = .true.
+            hit_record%t     = root
+            hit_record%point = ray%at( root )
+            outer_normal     = ( hit_record%point - self%center ) / self%radius
+
+            call hit_record%set_face_normal( ray, outer_normal )
+
+        end block
 
     end procedure hit_sphere
 
