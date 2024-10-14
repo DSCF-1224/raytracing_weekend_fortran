f3e6225c2aeb068a206eac6916e829f9fe0c4719
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-14 22:20:08 +0900

    Update `MODULE` `raytracing_hittable_list_item` - Update the definition of `hittable_list_item_type` as `extends( hittable_class )` - Introduce `INTERFACE` `hit` for `hittable_list_item_type` - Introduce `MODULE` `raytracing_hit_record` - Introduce `MODULE` `raytracing_ray`

--- src/HittableListItem/module.f90
+++ src/HittableListItem/module.f90
@@ -1,6 +1,9 @@
 module raytracing_hittable_list_item
 
+    use,     intrinsic :: iso_fortran_env
+    use, non_intrinsic :: raytracing_hit_record
     use, non_intrinsic :: raytracing_hittable
+    use, non_intrinsic :: raytracing_ray
 
 
 
@@ -13,10 +16,38 @@ module raytracing_hittable_list_item
 
 
 
-    type :: hittable_list_item_type
+    type, extends( hittable_class ) :: hittable_list_item_type
 
         class(hittable_class), private, allocatable :: object
 
+
+
+        contains
+
+
+
+        procedure, pass, public :: hit => hit_hittable_list_item
+
     end type hittable_list_item_type
+
+
+
+    interface
+
+        module subroutine hit_hittable_list_item( self, ray, t_min, t_max, hit_stat, hit_record )
+
+            class(hittable_list_item_type), intent(in) :: self
+
+            type(ray_type), intent(in) :: ray
+
+            real(real64), intent(in) :: t_min, t_max
+
+            logical, intent(out) :: hit_stat
+
+            type(hit_record_type), intent(out) :: hit_record
+
+        end subroutine hit_hittable_list_item
+
+    end interface
 
 end module raytracing_hittable_list_item
