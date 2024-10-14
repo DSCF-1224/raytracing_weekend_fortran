7ddfddfaf25259f31ec665c24625faba27b820c5
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-14 17:13:49 +0900

    Update new `MODULE` `raytracing_hittable_list_item` - Add the definition `hittable_list_item_type`

--- src/HittableListItem/module.f90
+++ src/HittableListItem/module.f90
@@ -1,2 +1,15 @@
 module raytracing_hittable_list_item
+
+    implicit none
+
+
+
+    private
+    public  :: hittable_list_item_type
+
+
+
+    type :: hittable_list_item_type
+    end type hittable_list_item_type
+
 end module raytracing_hittable_list_item
