fa2d739817abdabf55bfbddd8edc97f404519c7c
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-18 06:55:25 +0900

    Update `MODULE` `raytracing_interval` - Add a new `TYPE` `interval_type`

--- src/Interval/module.f90
+++ src/Interval/module.f90
@@ -1,2 +1,15 @@
 module raytracing_interval
+
+    implicit none
+
+
+
+    private
+    public  :: interval_type
+
+
+
+    type :: interval_type
+    end type interval_type
+
 end module raytracing_interval
