b394cb0f86767bf3bfc6e3c2c308aa60d489f406
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-20 08:35:29 +0900

    Update Implementation: Image 05 (mod1) (WIP) - Import `MODULE` `raytracing_camera` - Update `SUBROUTINE` `render_image05_mod1` (cannot run)

--- src/InOneWeekend/render_image05_mod1.f90
+++ src/InOneWeekend/render_image05_mod1.f90
@@ -1,5 +1,6 @@
 submodule (raytracing_in_one_weekend) imp_render_image05_mod1
 
+    use, non_intrinsic :: raytracing_camera
     use, non_intrinsic :: raytracing_color
     use, non_intrinsic :: raytracing_hit_record
     use, non_intrinsic :: raytracing_hittable
@@ -81,71 +82,9 @@ submodule (raytracing_in_one_weekend) imp_render_image05_mod1
 
     module procedure render_image05_mod1
 
-        ! Image
-
-        real(real64) , parameter :: aspect_ratio = 16.0_real64 / 9.0_real64
-        integer      , parameter :: image_width  = 400;
-
-        integer, parameter :: image_height_ini = int( image_width / aspect_ratio )
-        !! calculated image height
-
-        integer, parameter :: image_height_min = 1
-
-        integer, parameter :: image_height = &!
-            merge( &!
-                tsource =                       image_height_min , &!
-                fsource = image_height_ini                       , &!
-                mask    = image_height_ini .lt. image_height_min   &!
-            )
-        !! calculated image height (ensured that it's at least 1)
-
-        integer, parameter :: iter_h_max   = image_height - 1
-        integer, parameter :: iter_w_max   = image_width  - 1
-
-
-
-        ! Camera
-
-        real(real64)    , parameter :: focal_length    = 1.0_real64
-        real(real64)    , parameter :: viewport_height = 2.0_real64
-        real(real64)    , parameter :: viewport_width  = viewport_height * aspect_ratio
-        type(vec3_type) , parameter :: camera_center   = vec3_type( x = 0.0_real64, y = 0.0_real64, z = 0.0_real64 )
-
-        type(vec3_type) , parameter :: viewport_u      = vec3_type( x = viewport_width , y =   0.0_real64      , z = 0.0_real64 )
-        type(vec3_type) , parameter :: viewport_v      = vec3_type( x = 0.0_real64     , y = - viewport_height , z = 0.0_real64 )
-
-
-
-        integer :: iter_h
-        !! A support variable for this `SUBROUTINE`
-
-        integer :: iter_w
-        !! A support variable for this `SUBROUTINE`
-
-        integer :: write_unit
-        !! the unit number to save the rendered image
+        type(camera_type) :: camera
 
         type(hittable_list_type) :: world
-
-        type(vec3_type) :: pixel00_loc
-        type(vec3_type) :: pixel_delta_u
-        type(vec3_type) :: pixel_delta_v
-        type(vec3_type) :: viewport_upper_left
-
-
-
-        pixel_delta_u = viewport_u / image_width
-        pixel_delta_v = viewport_v / image_height
-
-        viewport_upper_left &!
-            = camera_center                                                 &!
-            - vec3_type( x = 0.0_real64, y = 0.0_real64, z = focal_length ) &!
-            - viewport_u / 2                                                &!
-            - viewport_v / 2
-
-        pixel00_loc &!
-            = viewport_upper_left &!
-            + 0.5_real64 * ( pixel_delta_u + pixel_delta_v )
 
 
 
@@ -169,74 +108,18 @@ submodule (raytracing_in_one_weekend) imp_render_image05_mod1
 
 
 
-        open( &!
-            newunit = write_unit         , &!
-            file    = 'image05_mod1.ppm' , &!
-            action  = 'write'            , &!
-            form    = 'formatted'        , &!
-            status  = 'unknown'            &!
+        call camera%initialize( &!
+            image_width  = 400                      , &!
+            aspect_ratio = 16.0_real64 / 9.0_real64   &!
         )
 
 
 
-        call write_ppm_image_header( &!
-            unit         = write_unit   , &!
-            image_width  = image_width  , &!
-            image_height = image_height   &!
+        call camera%render( &!
+            file      = 'image05_mod1.ppm', &!
+            world     = world             , &!
+            ray_color = ray_color           &!
         )
-
-
-
-        do iter_h = 0, iter_h_max
-
-            ! Write progress bar
-
-            write( unit = output_unit, fmt = '(A,A,I3,A)', advance = 'no' ) &!
-                char(13)                                                , &!
-                'Scan lines remaining: '                                , &!
-                floor( 100 * real( iter_h_max - iter_h ) / iter_h_max ) , &!
-                '%'
-
-            flush( unit = output_unit )
-
-
-
-            do iter_w = 0, iter_w_max
-            block
-
-                type(color_type) :: color
-                type(ray_type)   :: ray
-                type(vec3_type)  :: pixel_center
-                type(vec3_type)  :: ray_direction
-
-
-
-                pixel_center &!
-                    = viewport_upper_left        &!
-                    + ( iter_w * pixel_delta_u ) &!
-                    + ( iter_h * pixel_delta_v )
-
-                ray_direction = pixel_center - camera_center
-
-                ray%origin    = camera_center
-                ray%direction = ray_direction
-
-                color = ray_color( ray, world )
-
-                write( unit = write_unit, fmt = * ) color
-
-            end block
-            end do
-
-        end do
-
-
-
-        write( unit = output_unit, fmt = '(/,A)', advance = 'yes' ) 'DONE !'
-
-
-
-        close( write_unit )
 
     end procedure render_image05_mod1
 
