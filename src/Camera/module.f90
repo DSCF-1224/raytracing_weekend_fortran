a954a60679511850ce31b3e7c3b891921c6e15f7
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-19 20:12:23 +0900

    Update `MODULE` `raytracing_camera` - Import a `MODULE` `iso_fortran_env` - Add fields of `camera_type`

--- src/Camera/module.f90
+++ src/Camera/module.f90
@@ -1,10 +1,24 @@
 module raytracing_camera
 
+    use, intrinsic :: iso_fortran_env
+
+
+
     implicit none
 
 
 
     type :: camera_type
+
+        integer, private :: image_height_
+        !! Rendered image height
+
+        integer, private :: image_width_ = 100
+        !! Rendered image width in pixel count
+
+        real(real64), private :: aspect_ratio_ = 1.0_real64
+        !! Ratio of image width over height
+
     end type camera_type
 
 end module raytracing_camera
