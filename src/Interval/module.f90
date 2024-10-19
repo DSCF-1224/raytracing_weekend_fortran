0e6fa713e8739a8e49fedae468d12e937b1751c8
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-19 18:07:00 +0900

    Update `MODULE` `raytracing_interval` - Add `FUNCTION` `surrounds` for `interval_type`

--- src/Interval/module.f90
+++ src/Interval/module.f90
@@ -18,6 +18,30 @@ module raytracing_interval
         real(real64), public :: max = - huge( 0.0_real64 )
         real(real64), public :: min =   huge( 0.0_real64 )
 
+
+
+        contains
+
+
+
+        procedure, pass, public :: surrounds
+
     end type interval_type
+
+
+
+    interface
+
+        module pure elemental function surrounds( interval, x )
+
+            class(interval_type), intent(in) :: interval
+
+            real(real64), intent(in) :: x
+
+            logical :: surrounds
+
+        end function surrounds
+
+    end interface
 
 end module raytracing_interval
