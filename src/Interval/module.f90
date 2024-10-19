7f0ec68578d1fd3c78b053a298866fb0a013b96e
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-19 11:35:25 +0900

    Update `MODULE` `module raytracing_interval` - Add fields of `TYPE` `interval_type`

--- src/Interval/module.f90
+++ src/Interval/module.f90
@@ -14,6 +14,10 @@ module raytracing_interval
 
 
     type :: interval_type
+
+        real(real64), public :: max = - huge( 0.0_real64 )
+        real(real64), public :: min =   huge( 0.0_real64 )
+
     end type interval_type
 
 end module raytracing_interval
