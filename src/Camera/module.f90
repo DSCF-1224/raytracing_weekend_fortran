76975bf3d3430252cdb6c2cb9a5f0017c34ef5a8
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-19 20:29:04 +0900

    Update `MODULE` `raytracing_camera` - Import a `MODULE` `raytracing_vec3` - Add fields of `camera_type`

--- src/Camera/module.f90
+++ src/Camera/module.f90
@@ -1,6 +1,7 @@
 module raytracing_camera
 
-    use, intrinsic :: iso_fortran_env
+    use,     intrinsic :: iso_fortran_env
+    use, non_intrinsic :: raytracing_vec3
 
 
 
@@ -18,6 +19,18 @@ module raytracing_camera
 
         real(real64), private :: aspect_ratio_ = 1.0_real64
         !! Ratio of image width over height
+
+        type(vec3_type), private :: center_
+        !! Camera center
+
+        type(vec3_type), private :: pixel00_loc_
+        !! Location of pixel 0, 0
+
+        type(vec3_type), private :: pixel_delta_u
+        !! Offset to pixel to the right
+
+        type(vec3_type), private :: pixel_delta_v
+        !! Offset to pixel below
 
     end type camera_type
 
