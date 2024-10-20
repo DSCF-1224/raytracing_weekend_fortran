063b20ae8356f6f8fcaea120f9f0ea05c37e513c
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2024-10-21 06:28:24 +0900

    Update `MODULE` `raytracing_random_number` - Add an `INTERRACE` for `random_number`

--- src/RandomNumber/module.f90
+++ src/RandomNumber/module.f90
@@ -1,2 +1,15 @@
 module raytracing_random_number
+
+    implicit none
+
+
+
+    private
+    public  :: random_number
+
+
+
+    interface random_number
+    end interface random_number
+
 end module raytracing_random_number
